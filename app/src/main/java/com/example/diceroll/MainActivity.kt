3c9b804dad475fe8b0659fcea27b3f0879819588
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2022-02-27 15:09:24 +0100

    Safe

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -12,6 +12,7 @@ import android.widget.AdapterView
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.diceroll.model.BEHistoryItem
 import kotlinx.android.synthetic.main.activity_history.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
@@ -19,18 +20,20 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     private val TAG: String = "xyz"
-    private var history = mutableListOf<Int>()
+    private var history = mutableListOf<BEHistoryItem>()
     private var amount: Int = 1
     private var dices = mutableListOf<ImageView>()
+    private val HISTORY = "HISTORY"
+    private val AMOUNT = "AMOUNT"
 
     private val diceId = intArrayOf(
-    0,
-    R.drawable.dice1,
-    R.drawable.dice2,
-    R.drawable.dice3,
-    R.drawable.dice4,
-    R.drawable.dice5,
-    R.drawable.dice6
+        0,
+        R.drawable.dice1,
+        R.drawable.dice2,
+        R.drawable.dice3,
+        R.drawable.dice4,
+        R.drawable.dice5,
+        R.drawable.dice6
     )
 
     private val mRandomGenerator = Random()
@@ -56,27 +59,57 @@ class MainActivity : AppCompatActivity() {
             override fun onNothingSelected(p0: AdapterView<*>?) {
             }
         }
+
+        if (savedInstanceState != null){
+            val pairs = savedInstanceState.getSerializable(HISTORY) as Array<BEHistoryItem>
+            history.addAll(pairs)
+            amount = savedInstanceState.getInt(AMOUNT)
+            if (history.isNotEmpty()){
+                for (i in 0..5){
+                    dices[i].visibility= View.INVISIBLE
+                }
+
+                for (i in 0..amount){
+                    val image = history.last()
+                    Log.d(TAG, "$image information")
+                    Log.d(TAG, "$history history")
+
+                    var number = 0
+                    when(i){
+                        0 ->{number = image.firstDice as Int;}
+                        1 ->{number = image.secondDice as Int;}
+                        2 ->{number = image.thirdDice as Int;}
+                        3 ->{number = image.fourthDice as Int;}
+                        4 ->{number = image.fifthDice as Int;}
+                        5 ->{number = image.sixthDice as Int;}
+                    }
+                    dices[i].visibility= View.VISIBLE
+                    updateDicesWith(number,i)
+                }
+            }
+        }
         btnRoll.setOnClickListener{ _ -> onClickRoll()}
     }
 
     private fun onClickRoll(){
         var sum: Int = 0
+        val numbers = arrayOf<Int>(0,0,0,0,0,0)
         for (i in 0..amount){
             val image = mRandomGenerator.nextInt(6) + 1
+            numbers[i] = image
             sum += image
             updateDicesWith(image, i)
         }
 
+        val record = BEHistoryItem(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5])
         Toast.makeText(this, "Total count: " + (sum) , Toast.LENGTH_SHORT).show()
         if (history.size >= 10){
             history.removeAt(0)
-            history.add(sum)
+            history.add(record)
         }else {
-            history.add(sum)
+            history.add(record)
         }
     }
-
-
 
     private fun updateDicesWith(image: Int, index: Int) {
         dices[index].setImageResource(diceId[image])
@@ -93,15 +126,15 @@ class MainActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.iHistory -> {
                 Intent(this, HistoryActivity::class.java).also {
-                    it.putExtra("EXTRA_HISTORY", history.toIntArray())
+                    it.putExtra("EXTRA_HISTORY", history.toTypedArray())
                     startActivity(it)
                 }
             }
-            R.id.iRules-> Intent(this, RulesActivity::class.java).also {
-                startActivity(it)
-            }
-            R.id.iSettings-> Intent(this,SettingsActivity::class.java).also {
-                startActivity(it)
+            R.id.iRules-> Toast.makeText(this, "Item 2 Selected", Toast.LENGTH_SHORT).show()
+            R.id.iSettings -> {
+                Intent(this, SettingsActivity::class.java).also {
+                    startActivity(it)
+                }
             }
             else -> {
                 return super.onOptionsItemSelected(item)
@@ -110,6 +143,9 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-
-
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable(HISTORY, history.toTypedArray())
+        outState.putSerializable(AMOUNT, amount)
+    }
 }
---
+++ app/src/main/java/com/example/diceroll/model/BEHistoryItem.kt
@@ -0,0 +1,5 @@
+package com.example.diceroll.model
+
+import java.io.Serializable
+
+data class BEHistoryItem(var firstDice: Number, var secondDice: Number, var thirdDice: Number, var fourthDice: Number, var fifthDice: Number, var sixthDice: Number): Serializable
